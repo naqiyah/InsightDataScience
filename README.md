760ed8714b96b89cbda49e7b7dea604f4227ccd1
naqiyah <lakdawala.n@husky.neu.edu>
2020-04-22 14:26:43 -0400

    editing the README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # InsightDataScience
+The consumer_complaints.py file is a file available in the SRC 
+The file is the logic behind the whole expected output 
+
+To run the project just go to the designated directory and run: bash run.sh
+
+It will automatically take the input file which is the complaints.csv and give the reports.csv as the output in the output folder
+
+The main logic behind the project was done by first of all : Seperating the columns required, Storing them individually and then processing them in the special structure of the dictionaries.
+ 
+																
--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -1,8 +1,10 @@
 
-#importing the csv file 
+#importing the csv file and some inbuilt functions like counters and math library for calculating the round off percentages
 from collections import Counter,OrderedDict 
+from datetime import datetime
 import math
 import csv  
+#The file was opened on the local computer thats why the path is mentioned , it can be done using the cli using the run.sh 
 file =  open('../input/complaints.csv', 'r')
 reader = csv.reader(file)
 
@@ -10,6 +12,7 @@ items = []  # put the rows in csv to a list
 Date_Product = []  # to have a tuple of aisle and dept ids
 mydict = {} # product as keys and list of above tuple as values in a dictionary
 
+#defining the main requirements in tuples
 Date, product, company, timestam = [],[],[],[]
 
 for row in reader:
@@ -21,29 +24,18 @@ for i  in range(1, len(items)):
     product.append(items[i][1].lower())
     company.append(items[i][7].lower())
 
-
-#print(Date)
-#print(product)
-#print(company)
-
 #seperating the year from the date 
-from datetime import datetime
-
 for i in range(0, len(Date)):
     my_string = Date[i]
     my_date = datetime.strptime(my_string, "%Y-%m-%d")
     my_year = my_date.year
     timestam.append(my_year)
-print(timestam)
-
 
 #Capturing the required main categories in the tuple
 for item1, item2,item3 in zip(timestam,product,company):
     Date_Product.append((item2, item1,item3))
 
-print(Date_Product)
-
-#Forming the dictionaries 
+#Forming a general dictionary which contains a dictionary of the years and Companies associated with the product/complains 
 for x in Date_Product:
     if x[0] in mydict.keys():
         if x[1] in mydict[x[0]].keys(): 
@@ -58,22 +50,17 @@ for x in Date_Product:
         year_dict[x[1]] = company_list
         mydict[x[0]] = year_dict
         
-print(mydict) 
-
-
+#Preparing to write the output file 
 a_file = open("../output/report.csv", "w")
 mydict = OrderedDict(sorted(mydict.items())) 
-print(mydict)
 writer = csv.writer(a_file)
+#Obtaining the year,no_of_complaints, no_of_companies
 for product_name, year_dict in mydict.items():
-#year,no_of_complaints, no_of_companies
     year_dict = OrderedDict(sorted(year_dict.items()))
     for year,company_list in year_dict.items():
         no_of_complaints = len(company_list)
-#no_of_companies = len(set(company_list))
         companydict = Counter(company_list)
         no_of_companies = len(companydict)
         percentage = math.ceil(max(companydict.values())/no_of_complaints*100)
         writer.writerow([product_name,year,no_of_complaints,no_of_companies,percentage])
-
 a_file.close()
